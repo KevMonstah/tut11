eabf6b83cc7500b184b9f9e677338d9c5a5dde14
Kevin Foley <kevmonstah@gmail.com>
2023-06-20 20:36:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/* this is last one using local storage, so deploy this one.
+don't want to deploy one with json server
+
+need to clean up dependencies too
+*/
